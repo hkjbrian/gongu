6a27df61f454b911125fcf6b946f7c3b051a2eb1
hankyungjun <hkjbrian@gmail.com>
2026-04-22 00:20:19 +0900

    fix: storeId @Column에 nullable = false 추가 (#20)

--- src/main/java/com/gongu/server/domain/store/entity/StoreAdmin.java
+++ src/main/java/com/gongu/server/domain/store/entity/StoreAdmin.java
@@ -20,7 +20,7 @@ public class StoreAdmin extends SoftDeleteEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "store_id")
+    @Column(name = "store_id", nullable = false)
     private Long storeId;
 
     @Column(nullable = false, unique = true)
