7fac7e15202316a9de9ef2e49d8fc197faae8a14
hankyungjun <hkjbrian@gmail.com>
2026-05-30 16:40:21 +0900

    fix: VerifyPaymentRequest.paymentId @NotNull → @NotBlank (#120)

--- src/main/java/com/gongu/server/domain/payment/dto/request/VerifyPaymentRequest.java
+++ src/main/java/com/gongu/server/domain/payment/dto/request/VerifyPaymentRequest.java
@@ -1,9 +1,10 @@
 package com.gongu.server.domain.payment.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record VerifyPaymentRequest(
         @NotNull @JsonProperty("order_id") Long orderId,
-        @NotNull @JsonProperty("payment_id") String paymentId
+        @NotBlank @JsonProperty("payment_id") String paymentId
 ) {}
