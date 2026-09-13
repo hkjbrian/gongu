f4f28a45bc4961f30548a0a33470d2f132f3a361
hankyungjun <hkjbrian@gmail.com>
2026-09-13 16:24:31 +0900

    test: 마스킹 미호출 검증 보강 (#209)

--- src/test/java/com/gongu/server/domain/payment/service/PaymentHistoryRecorderTest.java
+++ src/test/java/com/gongu/server/domain/payment/service/PaymentHistoryRecorderTest.java
@@ -14,8 +14,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,5 +53,6 @@ class PaymentHistoryRecorderTest {
         ArgumentCaptor<PaymentHistory> captor = ArgumentCaptor.forClass(PaymentHistory.class);
         verify(paymentHistoryRepository).save(captor.capture());
         assertThat(captor.getValue().getPgRawResponse()).isNull();
+        verify(pgResponseMasker, never()).mask(any());
     }
 }
